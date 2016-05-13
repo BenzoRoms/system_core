340997a6eda4059f3f184bbd15ae200466fa6d30
William Roberts <william.c.roberts@intel.com>
2016-05-13 11:19:42 -0700

    fs_mgr: pass sehandle to ext4 format routine
    When fs_mgr_do_format() is executed, fs_mgr creates a new
    blank data file system. However, that filesystem is not
    labeled and causes some unlabeled denials on early boot.
    
    Example Denial:
    avc: denied { search } for pid=2535 comm="logd" name="/" dev="mmcblk0p9" ino=2 scontext=u:r:logd:s0 tcontext=u:object_r:unlabeled:s0 tclass=dir permissive=1
    
    To correct this, pass sehandle to the internal ext4 routine.
    This way the ext4 filesystem will be labeled at creation and
    the root inode will have a label.
    
    Change-Id: Ieeecaa8bbc258e6d743d281dd956bdaca98b365f
    Signed-off-by: William Roberts <william.c.roberts@intel.com>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- fs_mgr/Android.mk
+++ fs_mgr/Android.mk
@@ -13,6 +13,7 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)/include \
 
 LOCAL_MODULE:= libfs_mgr
 LOCAL_STATIC_LIBRARIES := liblogwrap libmincrypt libext4_utils_static libsquashfs_utils
+LOCAL_STATIC_LIBRARIES += libselinux
 LOCAL_C_INCLUDES += system/extras/ext4_utils system/extras/squashfs_utils
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/include
 LOCAL_CFLAGS := -Werror
