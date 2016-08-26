90e0558c1429bef6daa518ce36e86576afabdfa7
John Reck <jreck@google.com>
2016-08-26 13:43:19 -0700

    Fix access check
    Fixes a bug where USE_CPUSET used access(F_OK)
    as a sign that it should try writing to the cpuset
    path. It needs to instead use access(W_OK) to
    ensure that it has write access as that's what
    it would try to open() with.
    
    And stop trying to repeatedly open a file that
    doesn't exist.
    
    Change-Id: Ib391ee6f08345051c48a986a732eda30143cb614
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- libcutils/sched_policy.c
+++ libcutils/sched_policy.c
@@ -115,7 +115,7 @@ static int add_tid_to_cgroup(int tid, int fd)
 
 static void __initialize(void) {
     char* filename;
-    if (!access("/dev/cpuctl/apps/tasks", F_OK)) {
+    if (!access("/dev/cpuctl/apps/tasks", W_OK)) {
         __sys_supports_schedgroups = 1;
 
 #if CAN_SET_SP_SYSTEM
@@ -142,7 +142,7 @@ static void __initialize(void) {
     }
 
 #ifdef USE_CPUSETS
-    if (!access("/dev/cpuset/tasks", F_OK)) {
+    if (!access("/dev/cpuset/tasks", W_OK)) {
 
         filename = "/dev/cpuset/foreground/tasks";
         fg_cpuset_fd = open(filename, O_WRONLY | O_CLOEXEC);
